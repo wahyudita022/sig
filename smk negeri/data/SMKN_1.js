d547721aef69af5d3c4ae9e75fcee31f741a0ac0
wahyudita022 <108764578+wahyudita022@users.noreply.github.com>
2022-07-06 04:30:54 +0700

    Add files via upload

---
+++ smk negeri/data/SMKN_1.js
@@ -0,0 +1,16 @@
+var json_SMKN_1 = {
+"type": "FeatureCollection",
+"name": "SMKN_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "sekolah": "SMK N 1 PAKIS AJI", "alamat": "Desa Suwawal Timur RT. 02 RW. 02 RT 2 RW 2 Suwawal Timur" }, "geometry": { "type": "Point", "coordinates": [ 110.73374311803191, -6.585337203389222 ] } },
+{ "type": "Feature", "properties": { "id": "1", "sekolah": "SMK N 1 KARIMUNJAWA", "alamat": "Desa Karimunjawa RT. 06 RW. 02 RT 6 RW 2 Karimunjawa" }, "geometry": { "type": "Point", "coordinates": [ 110.446017355347948, -5.852589372381413 ] } },
+{ "type": "Feature", "properties": { "id": "1", "sekolah": "SMK N 1 JEPARA", "alamat": "Kelurahan Mulyoharjo RT. 03 RW. 05 RT 3 RW 5 Mulyoharjo" }, "geometry": { "type": "Point", "coordinates": [ 110.667379144864256, -6.56032184345203 ] } },
+{ "type": "Feature", "properties": { "id": "2", "sekolah": "SMK N 2 JEPARA", "alamat": "Kelurahan Pengkol RT. 04 RW. 02 RT 4 RW 2 Pengkol" }, "geometry": { "type": "Point", "coordinates": [ 110.688659143962624, -6.571305068793123 ] } },
+{ "type": "Feature", "properties": { "id": "3", "sekolah": "SMK N 3 JEPARA", "alamat": "Kelurahan Demaan RT. 01 RW. 02 RT 1 RW 2 Demaan Kode" }, "geometry": { "type": "Point", "coordinates": [ 110.670124951199526, -6.589152809972397 ] } },
+{ "type": "Feature", "properties": { "id": "1", "sekolah": "SMK N 1 KALINYAMATAN", "alamat": "Desa Sendang RT 0 RW 0 Sendang" }, "geometry": { "type": "Point", "coordinates": [ 110.699642369303717, -6.73399409415805 ] } },
+{ "type": "Feature", "properties": { "id": "1", "sekolah": "SMK N 1 KEDUNG", "alamat": "Desa Dongos RT. 04 RW. 04 RT 4 RW 4 Dongos" }, "geometry": { "type": "Point", "coordinates": [ 110.660514629026068, -6.667408290527679 ] } },
+{ "type": "Feature", "properties": { "id": "1", "sekolah": "SMK N 1 BATEALIT", "alamat": "Desa Bringin RT. 06 RW. 02 RT 6 RW 2 Bringin" }, "geometry": { "type": "Point", "coordinates": [ 110.744261722251906, -6.64338248509404 ] } },
+{ "type": "Feature", "properties": { "id": "1", "sekolah": "SMK N 1 BANGSRI", "alamat": "Desa Bangsri RT. 05 RW. 16 RT 5 RW 16 Bangsri" }, "geometry": { "type": "Point", "coordinates": [ 110.772406237188449, -6.55277087603003 ] } }
+]
+}
